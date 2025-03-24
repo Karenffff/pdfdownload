786e519f03bbf55a86e55c5be004f0da83ddeb67
Karenffff <tunjipete@grrnberry.com>
2025-03-24 07:34:21 +0100

    added procifle

--- build.sh
+++ build.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-python3.9 -m pip install -r requirements.txt
-echo "building the project"
-playwright install chromium
+pip install -r requirements.txt
+playwright install
 playwright install-deps
-python3.9 -m venv venv
-source venv/bin/activate
-python3.9 -m pip install -r requirements.txt
-
-echo "make migration.."
-python3.9 manage.py makemigrations --noinput
-python3.0 manage.py migrate --noinput
-
-echo "collect static..."
-python3.9 manage.py collectstatic --noinput --clear
+python manage.py migrate
+python manage.py collectstatic --noinput
